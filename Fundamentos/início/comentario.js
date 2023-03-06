36d47c5e8bf369c420d6156960e0ccedcf6b5533
Lucas Matheus da Cruz <dacruz@Lucass-MacBook-Air.local>
2023-03-06 15:50:08 -0300

    Primeiros passos

---
+++ Fundamentos/início/comentario.js
@@ -0,0 +1,21 @@
+// A ordem das sentenças importam em alguns casos;
+// console.log é usado para exibir algo na tela!
+
+console.log('Texto do curso');
+console.log('Step #01'); //exemplo de console.log
+console.log('Step #02');
+// console.log('Step #03');
+
+
+/*
+
+*Esse 
+*é 
+*um 
+*comentário 
+*de 
+*múltiplas 
+*linhas 
+
+
+*/
---
+++ Fundamentos/início/sentenca_1.js
@@ -0,0 +1,4 @@
+console.log("udemy");
+console.log("Bom dia");
+console.log("Boa tarde");
+console.log("Boa noite");
